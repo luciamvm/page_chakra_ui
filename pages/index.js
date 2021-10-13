785b6f080edcbc88741f411367ab20923985c41a
luciamvm <lucia23moita@gmail.com>
2021-10-13 21:34:00 +0100

    testing the grid

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Center, Flex, Heading, Spacer} from '@chakra-ui/layout';
+import { Box, BoxProps, Center, Flex, Grid, Heading, Spacer} from '@chakra-ui/layout';
 import { Image } from "@chakra-ui/react";
 
 
@@ -10,7 +10,19 @@ export default function Home() {
       <Image src='/firstImage.jpg' alt="Header Image" width="1215" height="645" opacity="0.53"/>
       <Box>
         <Heading mt={100} align="center" fontSize="40" fontFamily="SegoeUi, Segoe UI" color="#4f60b6">Includes the following</Heading>
+        <Grid templateColumns="repeat(2, 1fr)">
+          <Box w="50%" text="i'm a text">
+            <text>I'm a text</text>
+          </Box>
+          
+          <Box w="50%" text="i'm a text">
+            <text>I'm a text</text>
+          </Box>
+            
+
+        </Grid>
       </Box>
+      
     </Box>
     
   </Flex>
